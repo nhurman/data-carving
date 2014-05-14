923c2ac477623f5c545d492f625950b702d91acb
miuhaki <miurahareaki@gmail.com>
2014-05-14 20:25:57 +0200

    DiagonalViewItem

---
+++ BitGrapher/ui/DiagonalViewItem.cpp
@@ -0,0 +1,21 @@
+#include "DiagonalViewItem.h"
+
+DiagonalViewItem::DiagonalViewItem()
+{
+}
+
+DiagonalViewItem::DiagonalViewItem(qreal x1, qreal y1, qreal x2, qreal y2, QPen pen, QGraphicsItem * parent) :
+    QGraphicsLineItem(x1, y1, x2, y2, parent)
+{
+    this->setPen(pen);
+}
+
+
+void DiagonalViewItem::mousePressEvent ( QMouseEvent * event ){
+    if (event->buttons() == Qt::LeftButton) {
+        //emit diagonalClicked(this);
+        std::cout << this->line().x1() << " " << this->line().y1() << " " << this->line().dx() << std::endl;
+        std::cout << "DEATH PENIS" << std::endl;
+
+    }
+}
---
+++ BitGrapher/ui/DiagonalViewItem.h
@@ -0,0 +1,26 @@
+#ifndef DIAGONALVIEWITEM_H
+#define DIAGONALVIEWITEM_H
+
+#include <QWidget>
+#include <QGraphicsLineItem>
+#include <QMouseEvent>
+#include <QPen>
+#include <iostream>
+
+
+class DiagonalViewItem : public QGraphicsLineItem
+{
+    Q_OBJECT
+
+public:
+    DiagonalViewItem();
+    DiagonalViewItem(qreal x1, qreal y1, qreal x2, qreal y2, QPen pen, QGraphicsItem * parent = 0);
+
+protected:
+    void mousePressEvent ( QMouseEvent * event );
+
+signals:
+    void diagonalClicked(DiagonalViewItem *d);
+};
+
+#endif // DIAGONALVIEWITEM_H
