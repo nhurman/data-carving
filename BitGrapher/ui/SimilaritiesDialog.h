2c86cc3d592600e2256a35de5b226fc08c094491
GPrev <gabriel.prevosto@insa-rennes.fr>
2014-05-24 12:51:37 +0200

    Added doxygen comments to SimilaritiesDialog

--- BitGrapher/ui/SimilaritiesDialog.h
+++ BitGrapher/ui/SimilaritiesDialog.h
@@ -9,38 +9,82 @@
 #include "algorithms/similarities.h"
 #include "ui/dumpcombobox.h"
 
+/// \class SimilaritiesDialog
+///
+/// \brief Dialog used to ask the user the parameters for the Similarities alogorithm.
+///
+/// \author Gabriel Prevosto
+///
+/// \date 24/05/2014
+
 class SimilaritiesDialog : public QDialog
 {
     Q_OBJECT
 public:
+
+    /// \brief Constructor for SimilaritiesDialog.
+    /// \param parent The parent Widget (optional).
+    /// \param ds The DumpSet in which the algorithm will be applied.
+    /// \param selectedDump Where the compared dump's name (the one to display) will be stored.
     explicit SimilaritiesDialog(QWidget *parent = 0, DumpSet* ds = NULL, QString* selectedDump = NULL);
-    // selectedDump : where the compared dump's name (the one to display) will be stored
+
+    /// \brief Displays the SimilaritiesDialog and returns the results of the algorithm.
+    /// \note For an easy use of this class, just use this method.
+    /// \param ds The DumpSet in which the algorithm will be applied.
+    /// \param selectedDump Where the compared dump's name (the one to display) will be stored.
+    /// \return An instance of Similarity with the results of the algorithm.
     static Similarities* getSimilarities(DumpSet* ds, QString* selectedDump = NULL);
+
+    /// \brief Finds the dump selected by the ComboBox with the given index.
+    /// \param index The index of the ComboBox to look at.
+    /// \return The dump selected by the ComboBox with the index index.
     Dump getDump(int const index) const;
+
+    /// \brief Gets the chosen minimum string size.
+    /// \return The minimum string size.
     int getMinSize() const;
 
 signals:
 
 public slots:
-    void refreshComboBoxes(int modifiedIndex); //modifiedIndex : index of the modified CB -1 to refresh all
+
+    /// \brief Call this when a comboBox's selection has changed. Prevents the selection of a same Dump twice.
+    /// \param modifiedIndex Index of the modified ComboBox. -1 to refresh all.
+    void refreshComboBoxes(int modifiedIndex);
+
+    /// \brief Call this when the user presses OK (runs the algorithm and closes the dialog).
     void processAndClose();
+
+    /// \brief Call this when the user presses Cancel (closes the dialog).
     void cancelAndClose();
+
+    /// \brief Call this when the user presses + (adds a ComboBox if possible).
     void addComboBox();
+
+    /// \brief Call this when the user presses - (removes a ComboBox if possible).
     void removeComboBox();
+
+    /// \brief Call this when the user presses the default button (Sets the size to its preferred value).
+    /// \note Uses preferredStringSize().
     void displayDefaultSize();
 
 private:
-    QVBoxLayout* m_layout;
-    QString* m_selectedDump;
-    DumpSet* m_dumpSet;
-    std::vector<DumpComboBox*> m_dumpCBs;
-    QSpinBox* m_minSizeSpinBox;
+    QVBoxLayout* m_layout; ///< The layout ofr this Dialog.
+    QString* m_selectedDump; ///< The Dump selected by the first ComboBox
+    DumpSet* m_dumpSet; ///< The working DumpSet
+    std::vector<DumpComboBox*> m_dumpCBs; ///< The ComboBoxes
+    QSpinBox* m_minSizeSpinBox; ///< The SpinBox that controls the minimum string size
 
+    /// \breif refreshes a single ComboBox.
+    /// \param index The index of the ComboBoox to refresh.
+    /// \note Used by refreshComboBoxes(int).
     void refreshComboBox(int const index);
+
+    /// \brief Processes the preferred minimum string size.
+    /// \return The preferred minimum string size.
     int preferredStringSize() const;
 
-    //the result is tored here, to be recovered after window closure
-    static Similarities* m_result;
+    static Similarities* m_result; ///< The result of the algorithm is stored here, to be recovered after window closure.
 };
 
 
